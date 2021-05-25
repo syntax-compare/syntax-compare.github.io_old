e296aeefd78fed9aaa14207de086bd2ca28ba510
sshim <mechurak@gmail.com>
2021-05-25 16:16:40 +0900

    Apply style to code block

--- src/components/ui/Contents.js
+++ src/components/ui/Contents.js
@@ -3,20 +3,31 @@ import SyntaxHighlighter from "react-syntax-highlighter";
 import { darcula } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const Contents = () => {
-  const codeStr =
-    'if __name__ == "__main__": \n\
-    parser = argparse.ArgumentParser() \n\
-    parser.add_argument("-p", "--page", default=1, help="target pagenumber", type=int) \n\
-    parser.add_argument("-c", "--count", default=3, help="the number of videos to fetch url", type=int) \n\
-    args = parser.parse_args() main(args)';
+  const codeStr = `if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--page", default=1, help="target pagenumber", type=int)
+    parser.add_argument("-c", "--count", default=3, help="the number of videos to fetch url", type=int)
+    args = parser.parse_args() main(args)`;
 
   return (
-    <div>
-      <h4>Contents</h4>
-      <h5>python</h5>
-      <SyntaxHighlighter language="python" style={darcula}>
-        {codeStr}
-      </SyntaxHighlighter>
+    <div className="contents">
+      <h4>category</h4>
+      <h4>python</h4>
+      <h4>kotlin</h4>
+
+      <h4>for loop</h4>
+      <div className="code-item">
+        <SyntaxHighlighter language="python" style={darcula}>
+          {codeStr}
+        </SyntaxHighlighter>
+        <p>additional description</p>
+      </div>
+      <div className="code-item">
+        <SyntaxHighlighter language="python" style={darcula}>
+          {codeStr}
+        </SyntaxHighlighter>
+        <p>another description</p>
+      </div>
     </div>
   );
 };
