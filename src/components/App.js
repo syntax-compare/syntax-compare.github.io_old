fe8b8926ed31ef1e75643d7b0112502533a932d0
sshim <mechurak@gmail.com>
2021-05-25 13:10:45 +0900

    Clean up project

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import "../scss/main.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Grammar Cheet Sheet</header>
+    </div>
+  );
+}
+
+export default App;
