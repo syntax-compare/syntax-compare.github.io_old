6c8bcaac13a4958164132025d3c77838512c1621
sshim <mechurak@gmail.com>
2021-05-25 13:57:04 +0900

    Introduce React Syntax Highlighter

--- src/components/ui/Contents.js
+++ src/components/ui/Contents.js
@@ -1,19 +1,22 @@
 import React from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { darcula } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const Contents = () => {
   const codeStr =
-    'if __name__ == "__main__": \
-    parser = argparse.ArgumentParser() \
-    parser.add_argument("-p", "--page", default=1, help="target pagenumber", type=int) \
-    parser.add_argument("-c", "--count", default=3, help="the number of videos to fetch url", type=int) \
-    args = parser.parse_args() main(args) \
-';
+    'if __name__ == "__main__": \n\
+    parser = argparse.ArgumentParser() \n\
+    parser.add_argument("-p", "--page", default=1, help="target pagenumber", type=int) \n\
+    parser.add_argument("-c", "--count", default=3, help="the number of videos to fetch url", type=int) \n\
+    args = parser.parse_args() main(args)';
 
   return (
     <div>
       <h4>Contents</h4>
       <h5>python</h5>
-      <p>{codeStr}</p>
+      <SyntaxHighlighter language="python" style={darcula}>
+        {codeStr}
+      </SyntaxHighlighter>
     </div>
   );
 };
